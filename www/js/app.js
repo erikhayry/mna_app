ee5ba204e5ead941c9e3d819cd7884ecf194b844
Erik Portin <erikportin@gmail.com>
2016-02-01 19:04:50 +0100

    mock func

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
+angular.module('mna', ['ionic', 'mna.controllers', 'mna.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -29,39 +29,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.result', {
-    url: '/result',
-    views: {
-      'tab-result': {
-        templateUrl: 'templates/tab-result.html',
+    .state('result', {
+        url: '/',
+        templateUrl: 'templates/result.html',
         controller: 'ResultCtrl',
-        controllerAs: 'vm'
-      }
-    }
-  })
-
-  .state('tab.preferences', {
-      url: '/preferences',
-      views: {
-        'tab-preferences': {
-          templateUrl: 'templates/tab-preferences.html',
-          controller: 'PreferencesCtrl',
-          controllerAs: 'vm'
-        }
-      }
+        controllerAs: 'vm' 
     })
-    
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/result');
+  $urlRouterProvider.otherwise('/result');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,90 +1,50 @@
-angular.module('starter.controllers', [])
+angular.module('mna.controllers', [])
 
-.controller('ResultCtrl', function($scope, $ionicPlatform, DataService) {
-    var vm = this;
-    vm.result = [];
-    vm.track = {};
+.controller('ResultCtrl', function($scope, $ionicPlatform, $timeout, DataService) {
+    var vm = this,
+        _isDevice = false;        
+    vm.album = null;
     vm.error = '';
     vm.isLoading = true;
-    
-    $scope.safeApply = function(fn) {
-        var phase = this.$root.$$phase;
-        if(phase == '$apply' || phase == '$digest') {
-            if(fn && (typeof(fn) === 'function')) {
-                fn();
-            }
-        } else {
-            this.$apply(fn);
-        }
-    };
-        
+           
     function success(data){
-        console.log('success!')
-        console.timeEnd('getAudio');
-        $scope.safeApply(function(){
-            vm.result = DataService.getPopularAlbums(data)
-            vm.isLoading = false;
-            console.log(vm.result[0].trackId)
-            getTrack(vm.result[2].trackId);
-        })
+        console.log('success!', data)
+        console.timeEnd('getNextAlbum');
+
+        vm.album = data;
+        vm.error = '';
+        vm.isLoading = false;
+
     }
     
     function error(error){
         console.error(error)
-        console.timeEnd('getAudio');        
-        $scope.$apply(function(){
-            vm.error = error;
-            vm.result = [];
-            vm.isLoading = false;
-        })
+        console.timeEnd('getNextAlbum');        
+
+        vm.album = null;
+        vm.error = error;
+        vm.isLoading = false;
+       
     }
-    
-    function getTrack(id){
-        console.log('Try getting track', id)
-        vm.isLoading = true;     
-        if(window.plugins && window.plugins.iOSAudioInfo){   
-            window.plugins.iOSAudioInfo.getTrack(function(data){
-                $scope.safeApply(function(){
-                    vm.error = '';
-                    vm.album = data[0];
-                    vm.isLoading = false;
-                })
-            }, error, id);
-        }
-        else {
-            console.log('Get Mock Album')
-            vm.isLoading = true;
-            DataService.getAlbumMock(id).then(function(data){
-                vm.error = '';
-                vm.album = data[0];
-                vm.isLoading = false;
-            })
-        }             
-    }
-    
-    vm.getAudio = function(){
-        console.log('Try getting Audio')
+       
+    vm.getNextAlbum = function(){
+        console.log('Try getting Album')
+        console.time('getNextAlbum');
         vm.isLoading = true;
         vm.error = '';
-        vm.result = [];
-        console.time('getAudio');
-        if(window.plugins && window.plugins.iOSAudioInfo){
-            console.log('Get Audio')
-            window.plugins.iOSAudioInfo.getTracks(success, error);       
-        }
-        else {
-            console.log('Get Mock Audio')
-            DataService.getPopularAlbumsMock().then(success, error)
-        } 
+        vm.album = null;
+        DataService.getNextAlbum().then(success, error)       
     }
    
     //init       
     document.addEventListener('deviceready', function () {
+        _isDevice = true;
         vm.getAudio();
     }, false);
-})
-
-.controller('PreferencesCtrl', function() {
-    var vm = this;
-    vm.preferences = {};
+    
+    $timeout(function(){
+        if(!_isDevice){
+            vm.getNextAlbum();
+        }
+    }, 2000)  
 })
